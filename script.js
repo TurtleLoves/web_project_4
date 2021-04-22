796bfcb050b677a5ed4a93a410b4d6034d16cf05
Myles Jenkins <mylesjenkins1@yahoo.com>
2021-04-22 00:07:35 -0400

    Fixed scroll/modal overlay

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let profile__name = document.querySelector(".profile__name");
-let profile__about = document.querySelector(".profile__about-me");
-let profile__edit = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
-let modal__name = document.querySelector(".modal__name");
-let modal__about = document.querySelector(".modal__about-me");
-let modal__close = document.querySelector(".modal__close-icon");
-let modal__form = document.querySelector(".modal__form");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about-me");
+let profileEdit = document.querySelector(".profile__edit-button");
+let modalOverlay = document.querySelector(".modal__overlay");
+let modalName = document.querySelector(".modal__name");
+let modalAbout = document.querySelector(".modal__about-me");
+let modalClose = document.querySelector(".modal__close-icon");
+let modalForm = document.querySelector(".modal__form");
 // let elements = document.querySelector(".elements");
 // let elements__heart = document.querySelector(".elements__heart");
 // const hearts = Array.from(document.querySelectorAll(".elements__heart"));
 
 function open() {
-    modal__name.value = profile__name.textContent;
-    modal__about.value = profile__about.textContent;
-    modal.classList.add("modal__display");
+    modalName.value = profileName.textContent;
+    modalAbout.value = profileAbout.textContent;
+    modalOverlay.classList.add("modal__overlay_open");
 }
 
 function close() {
-    modal.classList.remove("modal__display");
+    modalOverlay.classList.remove("modal__overlay_open");
 }
 
 function submit(evt) {
   evt.preventDefault();
-  profile__name.textContent = modal__name.value;
-  profile__about.textContent = modal__about.value;
+  profileName.textContent = modalName.value;
+  profileAbout.textContent = modalAbout.value;
   close();
 }
 
@@ -31,9 +31,9 @@ function submit(evt) {
 //   evt.target.classList.toggle("elements__heart_active");
 // }
 
-profile__edit.addEventListener("click", open);
-modal__close.addEventListener("click", close);
-modal__form.addEventListener("submit", submit);
+profileEdit.addEventListener("click", open);
+modalClose.addEventListener("click", close);
+modalForm.addEventListener("submit", submit);
 
 // for (const heart of hearts) {
 //     heart.addEventListener("click", toggleHeart);
